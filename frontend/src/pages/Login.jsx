b9bb0e420240ffde0e1846bf0b8bfa766ad4ef63
Arkaprava23 <arkaprava.digitalexpeditor@gmail.com>
2024-07-07 20:22:35 +0530

    setup

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div className="w-full h-screen flex items-center justify-center">
+            <div className="flex">
+                hello
+            </div>
+        </div>
+    );
+};
+
+export default Login;
