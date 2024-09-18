ab736223f8c978d2a6ff1c0d04be44bc7958c21d
A. Gusti Putra <a.gusti.putra@accenture.com>
2024-09-18 21:25:27 +0700

    commit

--- src/utils/info.js
+++ src/utils/info.js
@@ -1,4 +1,5 @@
 //export const backendServer = "http://localhost:5050";   
 
 
-export const backendServer = "http://137.184.73.216:5000";
+//export const backendServer = "http://137.184.73.216:5000";
+export const backendServer = "http://159.223.189.6:5000";
